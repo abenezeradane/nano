a3a94613fb71f3bdf918fbb4cb6c6b3ff29367ab
PB020 <abe05081995@gmail.com>
2022-08-02 18:22:24 -0400

    Complete System Overhaul Pt. 2: Atlas

--- src/main/nano/Atlas.h
+++ src/main/nano/Atlas.h
@@ -1,16 +1,17 @@
-#ifndef ATLAS_H
-#define ATLAS_H
+#ifndef COMPONENTATLAS_H
+#define COMPONENTATLAS_H
 
-typedef enum bool {false, true} bool;
-
-typedef unsigned short Entity;
+#define MAX_SYSTEMS 2
 #define MAX_ENTITIES 4096
-#define MAX_COMPONENTS 16
+#define MAX_COMPONENTS 6
+typedef unsigned int Entity;
 
 #include "components/Component.h"
-#include "components/Health.h"
-#include "components/Sprite.h"
-#include "components/Position.h"
-#include "components/Velocity.h"
+#include "components/Camera.h"
+#include "components/Shader.h"
+#include "components/Texture.h"
+#include "components/Gravity.h"
+#include "components/Rigidbody.h"
+#include "components/Transform.h"
 
 #endif
