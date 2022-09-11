d0cadbc52682d227c9b1c2faafe049329938b388
PB020 <abe05081995@gmail.com>
2022-09-10 20:00:09 -0400

    Refactored project

--- src/main/nano/Systems.h
+++
@@ -1,8 +0,0 @@
-#ifndef SYSTEMATLAS_H
-#define SYSTEMATLAS_H
-
-#include "systems/Sprite.h"
-#include "systems/Physics.h"
-#include "systems/Controller.h"
-
-#endif
--- src/main/nano/Utility.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILITY_H
-#define UTILITY_H
-
-#include "util/Parser.h"
-#include "util/Queue.h"
-#include "util/external/stb_image.h"
-
-#endif
